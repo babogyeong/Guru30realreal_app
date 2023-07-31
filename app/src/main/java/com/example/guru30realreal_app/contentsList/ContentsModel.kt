9d4cb9b3665619b27c00c5e8950e3374d9862f3e
Lee Chaeyoung <0929alice@naver.com>
2023-07-31 13:21:19 +0900

    ContextModel.kt에 var webUrl추가

--- app/src/main/java/com/example/guru30realreal_app/contentsList/ContentsModel.kt
+++ app/src/main/java/com/example/guru30realreal_app/contentsList/ContentsModel.kt
@@ -2,5 +2,6 @@ package com.example.guru30realreal_app.contentsList
 
 data class ContentsModel (
      var title : String="",
-var imageUrl : String =""
-        )
+     var imageUrl : String ="",
+     var webUrl : String = ""
+)
