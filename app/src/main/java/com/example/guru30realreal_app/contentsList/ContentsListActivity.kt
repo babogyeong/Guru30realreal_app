3c10eb436a15b965ea9f7acc4e7cd6808eabd805
Lee Chaeyoung <0929alice@naver.com>
2023-07-31 12:52:49 +0900

    북마크 썸네일 이미지 삽입 완료

--- app/src/main/java/com/example/guru30realreal_app/contentsList/ContentsListActivity.kt
+++ app/src/main/java/com/example/guru30realreal_app/contentsList/ContentsListActivity.kt
@@ -19,9 +19,9 @@ class ContentsListActivity : AppCompatActivity() {
         val rv : RecyclerView = findViewById(R.id.rv)
 
         val items = ArrayList<ContentsModel>()
-        items.add(ContentsModel("imageUrl1", "title1"))
-        items.add(ContentsModel("imageUrl2", "title2"))
-        items.add(ContentsModel("imageUrl3", "title3"))
+        items.add(ContentsModel("https://images.mypetlife.co.kr/content/uploads/2023/03/02115658/AdobeStock_19383271-1536x1024.jpeg", "title1"))
+        items.add(ContentsModel("https://mypetlife.co.kr/wp-content/uploads/2019/06/adorable-animal-backyard-247937-1024x683.jpg", "title2"))
+        items.add(ContentsModel("https://images.mypetlife.co.kr/content/uploads/2020/05/09150624/12ecb492d8b24c03dc5af9da09e05678.jpg", "title3"))
 
         val rvAdapter = ContentsRVAdapter(items)
         rv.adapter = rvAdapter
