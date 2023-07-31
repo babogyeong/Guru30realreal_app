ac31ebbd9a7871b1d673b14be80be7c9e325830a
Lee Chaeyoung <0929alice@naver.com>
2023-07-31 15:18:22 +0900

    파이어베이스에 데이터 추가 후 확인

--- app/src/main/java/com/example/guru30realreal_app/contentsList/ContentsListActivity.kt
+++ app/src/main/java/com/example/guru30realreal_app/contentsList/ContentsListActivity.kt
@@ -23,15 +23,19 @@ class ContentsListActivity : AppCompatActivity() {
 
         // Write a message to the database
         val database = Firebase.database
-        val myRef = database.getReference("message")
+        val myRef = database.getReference("contents")
 
-        myRef.setValue("Hello, World!")
+        //데이터가 파이어베이스에 잘 들어가는지 확인 후 주석처리함.
+        /*myRef.push().setValue(
+            ContentsModel("title1", "https://images.mypetlife.co.kr/content/uploads/2023/03/02115658/AdobeStock_19383271-1536x1024.jpeg","https://mypetlife.co.kr/144708/")
+        )*/
 
 
         val rv : RecyclerView = findViewById(R.id.rv)
 
         //북마크에 띄울 아이템 생성(ContentsModel바탕으로 함.)
         val items = ArrayList<ContentsModel>()
+        /* 데이터가 파이어베이스에 잘 들어가는지 확인 후 주석처리함.
         items.add(ContentsModel("title1", "https://images.mypetlife.co.kr/content/uploads/2023/03/02115658/AdobeStock_19383271-1536x1024.jpeg","https://mypetlife.co.kr/144708/"))
         items.add(ContentsModel("title2", "https://mypetlife.co.kr/wp-content/uploads/2019/06/adorable-animal-backyard-247937-1024x683.jpg","https://mypetlife.co.kr/32243/"))
         items.add(ContentsModel("title3", "https://images.mypetlife.co.kr/content/uploads/2020/05/09150624/12ecb492d8b24c03dc5af9da09e05678.jpg","https://mypetlife.co.kr/87306/"))
@@ -44,7 +48,7 @@ class ContentsListActivity : AppCompatActivity() {
         items.add(ContentsModel("title10", "https://images.mypetlife.co.kr/content/uploads/2023/07/24193039/AdobeStock_273330906-1536x1024.jpeg","https://mypetlife.co.kr/149027/"))
         items.add(ContentsModel("title11", "https://images.mypetlife.co.kr/content/uploads/2022/04/18113500/florencia-potter-yxmNWxi3wCo-unsplash-edited-scaled.jpg","https://mypetlife.co.kr/114046/"))
         items.add(ContentsModel("title12", "https://images.mypetlife.co.kr/content/uploads/2022/04/12142152/%EC%8A%AC%EA%B0%9C%EA%B3%A8.png","https://mypetlife.co.kr/113673/"))
-
+*/
         val rvAdapter = ContentsRVAdapter(baseContext, items)
         rv.adapter = rvAdapter
 
